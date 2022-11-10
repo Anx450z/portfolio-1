2b3e3f1d513b3efebd022f36e0d4f877fe8ecfc5
Ankur <anx450z@gmail.com>
2022-11-10 10:29:10 +0530

    Fixed some css issues

--- src/componnets/portfolio/Portfolio.tsx
+++ src/componnets/portfolio/Portfolio.tsx
@@ -3,7 +3,19 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My recent work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className='portfolio_item'>
+          <div className="portfolio__item-image">
+            <h3>This is portfolio item title</h3>
+            <a href="https://github.com/anx450z" target='_blank'>Github</a>
+            <a href="#" target='_blank'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
